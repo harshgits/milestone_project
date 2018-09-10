4564471c49662cc21310832bc672b1faa4293e99
harshgits <chaturvediharshwardhan@gmail.com>
2018-09-10 02:08:02 -0400

    new

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Load the packages
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
@@ -57,75 +58,74 @@ def sparse_mask(n_orig, n_keep):
 # Create the main plot
 def create_figure(ticker, ticker_df = ticker_df):
 
-	## plot
+    ## plot
 
-	# query data
-	df = quandl.get("EOD/" + ticker)
+    # query data
+    df = quandl.get("EOD/" + ticker)
 
-	# make col names and index lowercase
-	# make index pd_datetime format
-	df.columns = map(str.lower, df.columns)
-	df.index.name = str.lower(df.index.name)
-	df.index = pddt(df.index)
+    # make col names and index lowercase
+    # make index pd_datetime format
+    df.columns = map(str.lower, df.columns)
+    df.index.name = str.lower(df.index.name)
+    df.index = pddt(df.index)
 
-	# keep only some rows from the df
-	start_date_str = '1900'
-	end_date_str = '2050'
-	df_sub = df[(df.index > pddt(start_date_str)) & (df.index < pddt(end_date_str))]
-	df_sub = df_sub[['close']]
+    # keep only some rows from the df
+    start_date_str = '1900'
+    end_date_str = '2050'
+    df_sub = df[(df.index > pddt(start_date_str)) & (df.index < pddt(end_date_str))]
+    df_sub = df_sub[['close']]
 
-	# sparsify
-	n_points = 40
-	df_sub = df_sub.iloc[sparse_mask(len(df_sub), n_points)]
+    # sparsify
+    n_points = 40
+    df_sub = df_sub.iloc[sparse_mask(len(df_sub), n_points)]
 
-	# diffs
-	df_sub['diff'] = df_sub['close'].diff()
-	df_sub['close_yest'] = df_sub['close'].shift()
-	fall_dates = df_sub.index[df_sub['diff'] < 0]
+    # diffs
+    df_sub['diff'] = df_sub['close'].diff()
+    df_sub['close_yest'] = df_sub['close'].shift()
+    fall_dates = df_sub.index[df_sub['diff'] < 0]
 
-	# make figure skeleton
-	p = figure(x_axis_type="datetime", title = ticker_df['name'][ticker])
-	p.yaxis.axis_label = "Price"
-	p.xaxis.axis_label = "Date"
-	p.xaxis.major_label_orientation = np.pi/4
-	p.grid.grid_line_alpha=0.3
+    # make figure skeleton
+    p = figure(x_axis_type="datetime", title = ticker_df['name'][ticker])
+    p.yaxis.axis_label = "Price"
+    p.xaxis.axis_label = "Date"
+    p.xaxis.major_label_orientation = np.pi/4
+    p.grid.grid_line_alpha=0.3
 
-	# candlestick width
-	width = (df_sub.index[-1] - df_sub.index[0]).total_seconds() / n_points * .7 \
-	    * 1000 # candlestick width in ms
-	    
-	# rises
-	rise_dates = df_sub.index[df_sub['diff'] > 0]
-	p.vbar(rise_dates, width, df_sub['close_yest'][rise_dates], df_sub['close'][rise_dates], 
-	       fill_color="dodgerblue", line_color="white") # rises
+    # candlestick width
+    width = (df_sub.index[-1] - df_sub.index[0]).total_seconds() / n_points * .7 \
+        * 1000 # candlestick width in ms
+        
+    # rises
+    rise_dates = df_sub.index[df_sub['diff'] > 0]
+    p.vbar(rise_dates, width, df_sub['close_yest'][rise_dates], df_sub['close'][rise_dates], 
+           fill_color="dodgerblue", line_color="white") # rises
 
-	# falls
-	fall_dates = df_sub.index[df_sub['diff'] < 0]
-	p.vbar(fall_dates, width, df_sub['close_yest'][fall_dates], df_sub['close'][fall_dates], 
-	       fill_color="coral", line_color="white") # rises
+    # falls
+    fall_dates = df_sub.index[df_sub['diff'] < 0]
+    p.vbar(fall_dates, width, df_sub['close_yest'][fall_dates], df_sub['close'][fall_dates], 
+           fill_color="coral", line_color="white") # rises
 
-	return p
+    return p
 
-# Index page
+#Connect the app
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def index():
 
-	# Determine the selected feature
-	current_ticker = request.args.get("ticker")
-	if current_ticker == None:
-		current_ticker = "AAPL"
+    # Determine the selected feature
+    current_ticker = request.args.get("ticker")
+    if current_ticker == None:
+        current_ticker = "AAPL"
 
-	# Create the plot
-	plot = create_figure(current_ticker)
-		
-	# Embed plot into HTML via Flask Render
-	script, div = components(plot)
-	return render_template("template.html", script=script, div=div,
-		tickers = tickers,  current_ticker = current_ticker)
+    # Create the plot
+    plot = create_figure(current_ticker)
+        
+    # Embed plot into HTML via Flask Render
+    script, div = components(plot)
+    return render_template("template.html", script=script, div=div,
+        tickers = tickers,  current_ticker = current_ticker)
 
-# With debug=True, Flask server will auto-reload 
-# when there are code changes
 if __name__ == '__main__':
-	app.run(debug = False)
+    app.run(debug=False)
+
